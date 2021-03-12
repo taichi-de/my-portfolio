677662f615ad610559873bd937c201118f73c5dc
taichi-de <61309248+taichi-de@users.noreply.github.com>
2021-03-12 18:05:51 +0100

    add ogp files

---
+++ pages/[dynamic]/index.ts
@@ -0,0 +1,15 @@
+<Head>
+  <meta
+    property="og:image"
+    key="ogImage"
+    content={`${baseUrl}/ogp/${dynamic}.png`}
+  />
+  <meta name="twitter:card" key="twitterCard" content="summary_large_image" />
+  <meta name="twitter:title" content={`これはテストだよ`} />
+  <meta name="twitter:description" content={"これはテストだよ"} />
+  <meta
+    name="twitter:image"
+    key="twitterImage"
+    content={`${baseUrl}/ogp/${dynamic}.png`}
+  />
+</Head>;
