5b3e3e0ddfe8e17fd9de0f1d7c8f2928291e148f
taichi-de <61309248+taichi-de@users.noreply.github.com>
2022-05-29 13:46:37 +0200

    tailwind update

--- pages/about.js
+++ pages/about.js
@@ -18,7 +18,11 @@ export default function About() {
           />
         </div>
         <div>
-          <h4>Hello! I am Taichi Tomioka</h4>
+          <h4>Hello! I am&nbsp;
+            <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-yellow-500 relative inline-block">
+              <span className="relative text-white font-serif font-bold">Taichi Tomioka</span>
+            </span>
+          </h4>
           <p>
             I'm from Japan and studied Agriculture in Akita(Northern Japan).
             <br />
