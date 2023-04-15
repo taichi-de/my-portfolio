7bde845d34ec580e5d45807bf44be046e4b54ec9
taichi-de <61309248+taichi-de@users.noreply.github.com>
2023-04-15 21:49:51 +0200

    added API type

--- libs/client.ts
+++ libs/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
-  apiKey: 'MICROCMS_API_KEY',
+  apiKey: process.env.MICROCMS_API_KEY,
   serviceDomain: 'taizen-dev',
 });
