6ec774a50d9aded261dc2d742867a2e6ab8a5d49
taichi-de <61309248+taichi-de@users.noreply.github.com>
2023-04-15 21:52:45 +0200

    setuped blog page

---
+++ pages/blog.tsx
@@ -0,0 +1,58 @@
+import { GetStaticProps, NextPage } from 'next';
+import { client } from '../libs/client';
+import { MicroCMSListResponse } from 'microcms-js-sdk';
+import Link from 'next/link';
+import BlogLayout from '../components/BlogLayout';
+import RightSidebar from '../components/blog/RightSidebar';
+
+export type Blog = {
+  id: string;
+  title: string;
+  body: string;
+};
+
+type Props = MicroCMSListResponse<Blog>;
+
+const Blog: NextPage<Props> = (props) => {
+  return (
+    <BlogLayout>
+      <div className="grid grid-cols-12 gap-4 p-8 w-4/5 h-4/5 text-left">
+        <div className="col-span-8 bg-gray-200 scrollbar-hide overflow-auto px-2">
+          <h1 className="text-center text-xl text-bold italic my-8">Archives</h1>
+          {/* <p className="text-gray-400">{`Articles amount: ${props.totalCount}`}</p> */}
+          <div className="grid grid-cols-2 gap-2">
+            {props.contents.map((content) => {
+              return (
+                <div className="col-span-1 border border-lime-600 p-4" key={content.id}>
+                  <div className="mx-auto text-sky-500 bg-gray-100 w-full h-40" />
+                  <Link
+                    className="text-base hover:text-blue-600 hover:underline"
+                    href={`/blog/${content.id}`}
+                  >
+                    <p className="my-4">{content.title}</p>
+                  </Link>
+                  <p className="text-sm m-0">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil deleniti
+                    laboriosam recusandae sint praesentium, reprehenderit odio dolores.
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="col-span-4 hidden lg:block h-screen overflow-y-scroll scrollbar-hide bg-sky-300 lg:bg-transparent">
+          <RightSidebar />
+        </div>
+      </div>
+    </BlogLayout>
+  );
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const data = await client.getList<Blog>({ endpoint: 'blog' });
+  return {
+    props: data,
+  };
+};
+
+export default Blog;
--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,17 +1,33 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import { client } from '../../libs/client';
-import { Blog } from '../blogpage';
+import { Blog } from '../blog';
 import { MicroCMSContentId, MicroCMSDate } from 'microcms-js-sdk';
+import dayjs from 'dayjs';
+import BlogLayout from '../../components/BlogLayout';
+import RightSidebar from '../../components/blog/RightSidebar';
 
 type Props = Blog & MicroCMSContentId & MicroCMSDate;
 
 const BlogId: NextPage<Props> = (props) => {
   return (
-    <>
-      <h1>{props.title}</h1>
-      <time>{props.publishedAt}</time>
-      <div dangerouslySetInnerHTML={{ __html: props.body }} />
-    </>
+    <BlogLayout>
+      <div className="grid grid-cols-12 gap-4 p-8 w-4/5 h-4/5">
+        <div className="col-span-8 p-8 bg-gray-200 scrollbar-hide overflow-auto">
+          <div className="w-full h-72 mx-auto bg-gray-400" />
+          <h1 className="text-xl font-bold my-8">{props.title}</h1>
+          <time dateTime={props.publishedAt} className="py-2 block">
+            {dayjs(props.publishedAt).format('YYYY/MM/DD')}
+          </time>
+          <div
+            className="prose prose-slate text-gray-300"
+            dangerouslySetInnerHTML={{ __html: props.body }}
+          />
+        </div>
+        <div className="col-span-4 hidden lg:block h-screen overflow-y-scroll scrollbar-hide bg-sky-300 lg:bg-transparent">
+          <RightSidebar />
+        </div>
+      </div>
+    </BlogLayout>
   );
 };
 
