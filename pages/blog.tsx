f41ac72accaa4035a583c8f0cd0e287ec43a8086
taichi-de <61309248+taichi-de@users.noreply.github.com>
2023-04-16 10:58:34 +0200

    fixed responsive design

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -17,13 +17,16 @@ const Blog: NextPage<Props> = (props) => {
   return (
     <BlogLayout>
       <div className="grid grid-cols-12 gap-4 p-8 w-4/5 h-4/5 text-left">
-        <div className="col-span-8 bg-gray-200 scrollbar-hide overflow-auto px-2">
+        <div className="lg:col-span-8 col-span-12 bg-gray-200 scrollbar-hide overflow-auto px-2">
           <h1 className="text-center text-xl text-bold italic my-8">Archives</h1>
           {/* <p className="text-gray-400">{`Articles amount: ${props.totalCount}`}</p> */}
           <div className="grid grid-cols-2 gap-2">
             {props.contents.map((content) => {
               return (
-                <div className="col-span-1 border border-lime-600 p-4" key={content.id}>
+                <div
+                  className="md:col-span-1 col-span-2 border border-lime-600 m-4 md:mb-0 p-4"
+                  key={content.id}
+                >
                   <div className="mx-auto text-sky-500 bg-gray-100 w-full h-40" />
                   <Link
                     className="text-base hover:text-blue-600 hover:underline"
